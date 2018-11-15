0c6f95b71432785e72d859a96aabfbae7df4839f
Shiyar Jamo <302264134@student.rocmondriaan.nl>
2018-11-15 15:00:23 +0100

    Level

---
+++ MyWorld.java
@@ -0,0 +1,58 @@
+
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ *
+ * @author R. Springer
+ */
+public class MyWorld extends World {
+
+    private CollisionEngine ce;
+
+    /**
+     * Constructor for objects of class MyWorld.
+     *
+     */
+    public MyWorld() {
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1000, 800, 1, false);
+        this.setBackground("startScreen.jpg");
+
+        //startScreen();
+        TileEngine te = new TileEngine(this, 60, 60);
+        // Declarenre en initialiseren van de camera klasse met de TileEngine klasse 
+        // zodat de camera weet welke tiles allemaal moeten meebewegen met de camera
+        Camera camera = new Camera(te);
+        // Declareren en initialiseren van een main karakter van het spel mijne heet Hero. Deze klasse 
+        // moet de klasse Mover extenden voor de camera om te werken
+        Hero hero = new Hero();
+
+        // Laat de camera een object volgen. Die moet een Mover instatie zijn of een extentie hiervan.
+        camera.follow(hero);
+
+        // Alle objecten toevoegen aan de wereld: camera, main karakter en mogelijke enemies
+        addObject(camera, 0, 0);
+
+        // Initialiseren van de CollisionEngine zodat de speler niet door de tile heen kan lopen.
+        // De collision engine kijkt alleen naar de tiles die de variabele solid op true hebben staan.
+        ce = new CollisionEngine(te, camera);
+        // Toevoegen van de mover instantie of een extentie hiervan
+        ce.addCollidingMover(hero);
+
+        prepare();
+    }
+
+    public void act() {
+        ce.update();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Start start = new Start();
+        addObject(start,518,403);
+    }
+}
